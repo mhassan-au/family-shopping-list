57a369bb30c2ae0e5acefcaf984b30f7cb53bef1
Amina Alam <hassan.mehedi.au@gmail.com>
2026-07-23 19:49:40 +1000

    Bug fix - removed unused notification tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import FamilyCodeScreen from "@/components/FamilyCodeScreen";
 import { hasDeviceLogin } from "@/lib/device";
 import ShoppingList from "@/components/ShoppingList";
 import AuthGate from "@/components/AuthGate";
-import EnableNotifications from "@/components/EnableNotifications";
 
 export default function Home() {
 
